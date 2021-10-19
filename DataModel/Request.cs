e15fb14b77e98337ccc67f30889012f6915e7eeb
daveyboy103@gmail.com <daveyboy103@gmail.com>
2021-10-19 20:23:48 +0100

    Code model created

---
+++ DataModel/Request.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataModel
+{
+    public class Request
+    {
+        public IEnumerable<Dimension> Dimensions { get; } = new List<Dimension>();
+        public IEnumerable<Measure> Measures { get; } = new List<Measure>();
+
+        public Dimension AddDimension(Dimension dimension)
+        {
+            (Dimensions as List<Dimension>)?.Add(dimension);
+            return dimension;
+        }
+
+        public Measure AddMeasure(Measure measure)
+        {
+            (Measures as List<Measure>)?.Add(measure);
+            return measure;
+        }
+    }
+
+    public class Measure
+    {
+        public IEnumerable<string> KeyColumns { get; } = new List<string>();
+        public IEnumerable<string> ValueColumns { get; } = new List<string>();
+        public IEnumerable<MeasureRow> Rows { get; } = new List<MeasureRow>();
+
+        public void AddKeyColumn(string column)
+        {
+            (KeyColumns as List<string>)?.Add(column);
+        }
+
+        public void AddValueColumn(string column)
+        {
+            (ValueColumns as List<string>)?.Add(column);
+        }
+
+        public MeasureRow AddRow(MeasureRow row)
+        {
+            (Rows as List<MeasureRow>)?.Add(row);
+            return row;
+        }
+    }
+
+    public class MeasureRow
+    {
+        public string DimensionId { get; init; }
+        public IEnumerable<string> Keys { get; } = new List<string>();
+        public IEnumerable<decimal> Measures { get; } = new List<decimal>();
+
+        public void AddKey(string key)
+        {
+            (Keys as List<string>)?.Add(key);
+        }
+        
+        public void AddMeasure(decimal measure)
+        {
+            (Measures as List<decimal>)?.Add(measure);
+        }
+    }
+
+    public class Dimension
+    {
+        public string DimensionId { get; init; }
+        public IEnumerable<string> Columns { get; } = new List<string>();
+        public IEnumerable<Row> Rows { get; } = new List<Row>();
+
+        public void AddColumn(string column)
+        {
+            (Columns as List<string>)?.Add(column);
+        }
+
+        public Row AddRow(Row row)
+        {
+            (Rows as List<Row>)?.Add(row);
+            return row;
+        }
+    }
+
+    public class Measures
+    {
+        public IEnumerable<KeyColumn> KeyColumns { get; } = new List<KeyColumn>();
+        public IEnumerable<ValueColumn> ValueColumns { get; } = new List<ValueColumn>();
+    }
+
+    public class ValueColumn
+    {
+    }
+
+    public class KeyColumn
+    {
+    }
+
+    public class Row
+    {
+        public string DimensionId { get; init; }
+        public IEnumerable<string> Dimensions { get; } = new List<string>();
+
+        public void AddDimension(string dimension)
+        {
+            (Dimensions as List<string>)?.Add(dimension);
+        }
+    }
+}
---
+++ Scratch/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using DataModel;
+
+namespace Scratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var request = new Request();
+            var dimension = request.AddDimension(new Dimension{DimensionId = "11"});
+            
+            dimension.AddColumn("report_name");
+            dimension.AddColumn("capitalunit");
+            dimension.AddColumn("capitalunit");
+            dimension.AddColumn("desk");
+            dimension.AddColumn("fundreference");
+            dimension.AddColumn("location");
+            dimension.AddColumn("strat");
+            dimension.AddColumn("strategy_category");
+            dimension.AddColumn("strategy_subcategory");
+            dimension.AddColumn("strategy_description");
+            dimension.AddColumn("strategy_owner");
+            dimension.AddColumn("tradequerysource");
+            dimension.AddColumn("ccy");
+            dimension.AddColumn("value.date");
+            dimension.AddColumn("deskgroupe");
+            dimension.AddColumn("capitalunit.isclosed");
+
+            var row = dimension.AddRow(new Row() { DimensionId = "11" });
+
+            row.AddDimension("All MT Dynamic PNL_fu_RMBASIS");
+            row.AddDimension("RM BASIS");
+            row.AddDimension("RM Basis");
+            row.AddDimension("Management Trading");
+            row.AddDimension("BC69");
+            row.AddDimension("JERSEY CHANNEL ISLANDS");
+            row.AddDimension("QRBUS");
+            row.AddDimension("Drics");
+            row.AddDimension(string.Empty);
+            row.AddDimension(string.Empty);
+            row.AddDimension("RM - Janis Drics");
+            row.AddDimension("Existing");
+            row.AddDimension("USD");
+            row.AddDimension( "29/09/2021 11:50:00");
+            row.AddDimension( "RM");
+            row.AddDimension( "0");
+            
+            row = dimension.AddRow(new Row() { DimensionId = "12" });
+
+            row.AddDimension("All MT Dynamic PNL_fu_RMBASIS");
+            row.AddDimension("RM BASIS");
+            row.AddDimension("RM Basis");
+            row.AddDimension("Management Trading");
+            row.AddDimension("BC69");
+            row.AddDimension("JERSEY CHANNEL ISLANDS");
+            row.AddDimension("QRBAU");
+            row.AddDimension("QRBAU");
+            row.AddDimension(string.Empty);
+            row.AddDimension(string.Empty);
+            row.AddDimension("RM - Cedric Levard");
+            row.AddDimension("Existing");
+            row.AddDimension("AUD");
+            row.AddDimension( "29/09/2021 11:50:00");
+            row.AddDimension( "RM");
+            row.AddDimension( "0");
+
+            var measure = new Measure();
+            measure.AddKeyColumn("key");
+            measure.AddValueColumn("value");
+            request.AddMeasure(measure);
+
+            var measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("deltaallparallel");
+            measureRow.AddMeasure(-348745.85337790177m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("fx.usd");
+            measureRow.AddMeasure(12m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("pnl");
+            measureRow.AddMeasure(-61152.874213245959m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("pnl.comp.date");
+            measureRow.AddMeasure(-208.50935653183842m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("pnl.comp.rate");
+            measureRow.AddMeasure(-106368.38167959411m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("pnl.comp.rate.usd");
+            measureRow.AddMeasure(-106368.38167959411m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("pnl.comp.rate.spread");
+            measureRow.AddMeasure(43972.388902395091m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("pv");
+            measureRow.AddMeasure(-122488.36675690848m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("pv.t0");
+            measureRow.AddMeasure(-122488.36675690848m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("size");
+            measureRow.AddMeasure(12881.0m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("pnl.comp.vol");
+            measureRow.AddMeasure(1451.6279204848979m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("pnl.comp.vol.vol");
+            measureRow.AddMeasure(1451.6279204848979m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "11"});
+            measureRow.AddKey("vega.norm");
+            measureRow.AddMeasure(-5336.3565863760014m);
+            // Dimension 12
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("deltaallparallel");
+            measureRow.AddMeasure(-54365.083254043479m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("fx.usd");
+            measureRow.AddMeasure(0.724m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("gammaallparallel");
+            measureRow.AddMeasure(2.6793425708776337m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("pnl");
+            measureRow.AddMeasure(21.415525617543608m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("pnl.comp.fx.aud");
+            measureRow.AddMeasure(21.415525617514504m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("pnl.comp.rate");
+            measureRow.AddMeasure(-208.50935653183842m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("pnl.comp.rate");
+            measureRow.AddMeasure(32934.58816668803m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("pnl.comp.rate.aud");
+            measureRow.AddMeasure(32934.58816668803m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("pnl.comp.spread");
+            measureRow.AddMeasure(-32934.58816668803m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("pv");
+            measureRow.AddMeasure(-155048.40547085978m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("pv.t0");
+            measureRow.AddMeasure(-155048.40547085978m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("size");
+            measureRow.AddMeasure(3050.0m);
+            measureRow = measure.AddRow(new MeasureRow{ DimensionId = "12"});
+            measureRow.AddKey("deltaallparallel");
+            measureRow.AddMeasure(382418.375242873090m);
+
+
+            Console.ReadKey();
+        }
+    }
+}
